a2b8c46163a4887969aa0dbc26736bf034c71019
khalidaBerki <khalidaberki7@gmail.com>
2026-03-30 22:09:18 +0200

    name correction

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,8 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Portfolio | Software Engineer - Backend & IA',
+  title: 'Portfolio | Software Engineer - Full Stack & DevOps',
   description: 'Développeuse Full Stack & IA spécialisée en automatisation intelligente, RAG et systèmes scalables.',
-  generator: 'v0.app',
   icons: {
     icon: [
       {
